0eb8ffa55726bd3d9a0255afa5e9e9cddd5946e2
watanaberenzi <tender_fake@yahoo.co.jp>
2024-08-02 13:11:11 +0900

    kadai

---
+++ kadai_005/score.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 四則演算（整数型のみ）
+        echo (80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60) / 10;
+        echo '<br>';
+        ?>
+    </p>
+</body>
+
+</html>
