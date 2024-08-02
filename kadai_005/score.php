3aa5a786552da66618b7f603e6c572113bd90e00
watanaberenzi <tender_fake@yahoo.co.jp>
2024-08-02 13:35:00 +0900

    修正

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -9,8 +9,18 @@
 <body>
     <p>
         <?php
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
         // 四則演算（整数型のみ）
-        echo (80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60) / 10;
+        echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 +$score8 + $score9 + $score10) / 10;
         echo '<br>';
         ?>
     </p>
