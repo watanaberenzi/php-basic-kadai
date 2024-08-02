44859c3ed12f87640ac4b103c6ecd43f5b9ca6a4
watanaberenzi <tender_fake@yahoo.co.jp>
2024-08-02 15:16:10 +0900

    kadai

---
+++ kadai_016/class.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP クラスとオブジェクト</title>
+</head>
+<body>
+    <p>
+        <?php
+        class Food {
+            private $name;
+            private $price;
+
+            public function __construct($name, $price) {
+                $this->name = $name;
+                $this->price = $price;
+            }
+
+            public function show_price() {
+                return $this->price;
+            }
+
+            public function __toString() {
+                return "Food Object ( [name:Food:private] => {$this->name} [price:Food:private] => {$this->price} )";
+            }
+        }
+
+        class Animal {
+            private $name;
+            private $height;
+            private $weight;
+
+            public function __construct($name, $height, $weight) {
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+            }
+
+            public function show_height() {
+                return $this->height;
+            }
+
+            public function __toString() {
+                return "Animal Object ( [name:Animal:private] => {$this->name} [height:Animal:private] => {$this->height} [weight:Animal:private] => {$this->weight} )";
+            }
+        }
+
+        $food = new Food("potato", 250);
+        $animal = new Animal("dog", 60, 5000);
+
+        echo $food . "<br>";
+        echo $animal . "<br>";
+
+        echo $food->show_price() . "<br>";
+        echo $animal->show_height();
+        ?>
+    </p>
+</body>
+</html>
